48da710bc03a5dd60d3e153dd8467f8a11b072e5
danieltmc <me@danieltmc.com>
2019-08-25 00:23:13 -0400

    Multiplication and addition of complex values added

--- Complex.java
+++ Complex.java
@@ -8,4 +8,18 @@ public class Complex
 		this.real = real;
 		this.imaginary = imaginary;
 	}
+	
+	public Complex times(Complex other)
+	{
+		double real = this.real * other.real - this.imaginary * other.imaginary;
+		double imaginary = this.real * other.imaginary + this.imaginary * other.real;
+		return(new Complex(real, imaginary));
+	}
+	
+	public Complex plus(Complex other)
+	{
+		double real = this.real + other.real;
+		double imaginary = this.imaginary + other.imaginary;
+		return(new Complex(real, imaginary));
+	}
 }
